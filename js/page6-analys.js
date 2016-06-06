9b1c75a256b1a0172167b8af29a911065df65fcf
07lyt <327442095@qq.com>
2016-06-06 14:05:58 +0800

    fds

--- js/page6-analys.js
+++ js/page6-analys.js
@@ -17,6 +17,7 @@ define(['jquery'],function($){
 		}
 		//console.log(data);
 		//get the data
+		var AROWNUMBER=4;
 		var iterate_data=data;
 		var ob={};		
 		for(var i=0;i<iterate_data.length;i++){
@@ -27,13 +28,21 @@ define(['jquery'],function($){
 		for(var level in ob){			
 			var classname="."+hashmap[level];
 			var length=ob[level].length;
+			var rownumber=Math.ceil(length/AROWNUMBER);			
+			var table=$(classname+" table tbody");
+			for(var j=0;j<rownumber;j++){
+				table.append($("<tr></tr>"));
+			}
+			$(classname).parent('div.hidep').last().show();
 			for(var i=0;i<length;i++){
-				$(classname).parent('p').last().show();
-				if(ob[level][i]["测评组分"]==='行为表征') $(classname).append($("<span>"+ob[level][i]["测评组分"]+"("+ob[level][i]["测评指标"]+")</span>"));
-				else $(classname).append($("<span>"+ob[level][i]["测评组分"]+"</span>"));
+				var row=Math.floor(i/AROWNUMBER)+1;			
+				if(ob[level][i]["测评组分"]==='行为表征'){$(classname+" table tbody tr:nth-of-type("+row+")").append($("<td>"+ob[level][i]["测评组分"]+"("+ob[level][i]["测评指标"]+")</td>"));}// 
+				else{
+					$(classname+" table tbody tr:nth-of-type("+row+")").append($("<td>"+ob[level][i]["测评组分"]+"</td>"));
+				}
 			}
 		}
-		console.log("ob: ",ob);
+		//console.log("ob: ",ob);
 		return ob;
 	}
 	return page6_analys;		
